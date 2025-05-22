b2502126292ae6b0434274e364dd3618aa046907
PVL-NBook <anakashalban@gmail.com>
2025-05-22 14:21:25 +0300

    task

---
+++ src/Stars.tsx
@@ -0,0 +1,24 @@
+import Star from "./Star";
+
+
+interface StarsProps {
+    starCount: number
+}
+
+
+function Stars({starCount}:StarsProps) {
+    let stars = [];
+    for (let i = 0; i < starCount; i++) {
+        stars.push(<Star />)
+    }
+    
+    
+    return (
+        <ul className="stars">
+            {stars}
+        </ul>
+    )
+}
+
+
+export default Stars
